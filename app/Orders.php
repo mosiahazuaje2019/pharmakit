360d02ea84ca43dbf393d2e19dbb91f0836c111c
Mosiah Azuaje <mosiahazuaje2010@gmail.com>
2020-07-17 16:42:03 -0500

    asi queda

--- app/Orders.php
+++ app/Orders.php
@@ -6,9 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Orders extends Model
 {
-<<<<<<< HEAD
     //Lo que sea a esto le sasdnajsdjasjdhaskjhdksjahdajskdh
-=======
-    //Modelo de ordenes
->>>>>>> cambios-001
 }
